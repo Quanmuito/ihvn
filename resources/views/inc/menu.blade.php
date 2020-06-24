5019d64c499cee310204566bba93e143d0a81b7a
Quanmuito <49555971+Quanmuito@users.noreply.github.com>
2020-06-24 11:04:17 +0300

    laravel ihvn

---
+++ resources/views/inc/menu.blade.php
@@ -0,0 +1,56 @@
+<header>       
+    <div class="nav-bar">
+        <nav>
+            <ul>
+                <li><a href="/">Trang chủ</a></li>
+                <li><a href="#">Giới thiệu &#8711</a>
+                    <ul>
+                        <li><a href="#">Tướng</a></li>
+                        <li><a href="#">Sự kiện</a></li>
+                        <li><a href="#">Trang bị &#8711</a>
+                            <ul>
+                                <li><a href="#">Thần khí</a></li>
+                                <li><a href="#">Đá quý</a></li>
+                                <li><a href="#">Trang phục</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Tài nguyên</a></li>
+                        
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+    <div class="logo">
+        <a href="{{ url('/') }}">
+            <img src="img/logo.png" alt="logo">
+        </a>
+    </div>
+
+    <div class="nav-bar">
+        <nav>
+            <ul>
+                <li><a href="#">Chơi game &#8711</a>
+                    <ul>
+                        <li><a href="#">Công trình</a></li>
+                        <li><a href="#">Bang hội</a></li>
+                        <li><a href="#">Hoạt động</a></li>
+                        <li><a href="#">Trang trí &#8711</a>
+                            <ul>
+                                <li><a href="#">Hóa trang</a></li>
+                                <li><a href="#">Khung</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="#">Hướng dẫn &#8711</a>
+                    <ul>
+                        <li><a href="#">Mới chơi</a></li>
+                        <li><a href="#">Bảng xếp hạng</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
